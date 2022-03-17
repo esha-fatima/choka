664b664da7f1c9c8510127a382a3579ccad92b69
esha-fatima <eshafatima2001@gmail.com>
2022-03-17 23:00:43 +0300

    initial group setup

---
+++ choka.com/app.js
@@ -0,0 +1,30 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const helmet = require("helmet");
+const ejs = require("ejs");
+const _= require("lodash");
+const firebase = require("firebase");
+const cookieParser = require('cookie-parser')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3000
+
+app.set('view engine','ejs');
+
+app.get("/home",(req,res)=>{
+    res.render("home");
+});
+
+app.get("/",(req,res)=>{
+    res.render("home");
+})
+
+
+app.listen(port, '0.0.0.0', ()=>{ // '0.0.0.0' is for running via docker only
+    // console.log("Server has started on port 3000");
+});
