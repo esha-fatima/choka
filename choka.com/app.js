57bf53ffb1be1d449b215cd7dfd08ceb8f2fd665
Talha <23100266@lums.edu.pk>
2022-04-14 02:40:44 +0500

    Admin Module

--- choka.com/app.js
+++ choka.com/app.js
@@ -1191,6 +1191,60 @@ app.post("/editProfile",(req,res)=>{
 
 });
 
+app.get('/adminSearchProfile',(req,res)=>{
+
+
+    // res.render("adminSearchProfile")
+    let obj1 =  {'Name': 'Student1', 'Subject': 'Physics', 'Experience': '3', 'Rating':'4.0', 'Image': 'xx'}
+    let obj2 =  {'Name': 'Student2', 'Subject': 'Sociology', 'Experience': '2', 'Rating':'1.0', 'Image': 'xx'}
+    let obj3 = {'Name': 'Student3', 'Subject': 'English', 'Experience': '6', 'Rating':'4.0', 'Image': 'xx'}
+    let results_array = [
+       obj1, obj2, obj3
+       ]
+   let ff = JSON.stringify(results_array)
+   console.log(ff)
+   let xx = {"bl":ff};
+   //console.log(res)
+   res.render("adminSearchProfile",xx)
+})
+
+app.get('/adminApproveProfile',(req,res)=>{
+
+
+    // res.render("adminSearchProfile")
+    let obj1 =  {'Name': 'Student1', 'Subject': 'Physics', 'Experience': '3', 'Rating':'4.0', 'Image': 'xx'}
+    let obj2 =  {'Name': 'Student2', 'Subject': 'Sociology', 'Experience': '2', 'Rating':'1.0', 'Image': 'xx'}
+    let obj3 = {'Name': 'Student3', 'Subject': 'English', 'Experience': '6', 'Rating':'4.0', 'Image': 'xx'}
+    let results_array = [
+       obj1, obj2, obj3
+       ]
+   let ff = JSON.stringify(results_array)
+   console.log(ff)
+   let xx = {"bl":ff};
+   //console.log(res)
+   res.render("adminApproveProfile",xx)
+})
+
+app.get('/adminDeleteProfile',(req,res)=>{
+
+
+    // res.render("adminSearchProfile")
+    let obj1 =  {'Name': 'Student1', 'Subject': 'Physics', 'Experience': '3', 'Rating':'4.0', 'Image': 'xx'}
+    let obj2 =  {'Name': 'Student2', 'Subject': 'Sociology', 'Experience': '2', 'Rating':'1.0', 'Image': 'xx'}
+    let obj3 = {'Name': 'Student3', 'Subject': 'English', 'Experience': '6', 'Rating':'4.0', 'Image': 'xx'}
+    let results_array = [
+       obj1, obj2, obj3
+       ]
+   let ff = JSON.stringify(results_array)
+   console.log(ff)
+   let xx = {"bl":ff};
+   //console.log(res)
+   res.render("adminDeleteProfile",xx)
+})
+
+app.get("/adminDash",(req,res)=>{
+    res.render("adminDash");
+});
 
 
 
