541483de5d359b39e873c6c89d3c4402773089e3
Talha <23100266@lums.edu.pk>
2022-04-15 06:07:39 +0500

    admin Dashboard Backend completed

--- choka.com/app.js
+++ choka.com/app.js
@@ -818,6 +818,40 @@ app.post('/adminSearchProfile', (req,res)=>{
         }
     })
 })
+app.get('/adminApproveProfile',(req,res)=>{
+
+
+    // res.render("adminSearchProfile")
+    let obj1 =  {'Name': 'Student1', 'Subject': 'Physics', 'Experience': '3', 'Rating':'4.0', 'Image': 'xx'}
+    let obj2 =  {'Name': 'Student2', 'Subject': 'Sociology', 'Experience': '2', 'Rating':'1.0', 'Image': 'xx'}
+    let obj3 = {'Name': 'Student3', 'Subject': 'English', 'Experience': '6', 'Rating':'4.0', 'Image': 'xx'}
+    let results_array = [
+       obj1, obj2, obj3
+       ]
+   let ff = JSON.stringify(results_array)
+   console.log(ff)
+   let xx = {"bl":ff};
+   //console.log(res)
+   res.render("adminApproveProfile",xx)
+})
+
+app.get('/adminDeleteProfile',(req,res)=>{
+
+
+    // res.render("adminSearchProfile")
+    let obj1 =  {'Name': 'Student1', 'Subject': 'Physics', 'Experience': '3', 'Rating':'4.0', 'Image': 'xx'}
+    let obj2 =  {'Name': 'Student2', 'Subject': 'Sociology', 'Experience': '2', 'Rating':'1.0', 'Image': 'xx'}
+    let obj3 = {'Name': 'Student3', 'Subject': 'English', 'Experience': '6', 'Rating':'4.0', 'Image': 'xx'}
+    let results_array = [
+       obj1, obj2, obj3
+       ]
+   let ff = JSON.stringify(results_array)
+   console.log(ff)
+   let xx = {"bl":ff};
+   //console.log(res)
+   res.render("adminDeleteProfile",xx)
+})
+
 ////////////////////rendering login
 app.post('/findTutors', (req,res)=>{
     //now u are supposed to get the top 3 tutors from database.
